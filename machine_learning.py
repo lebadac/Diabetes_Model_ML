2fba63b67a1e52c20ffb5b0c67901e8a1cebe097
Ba Dac <lebadac@Bas-MacBook-Pro.local>
2025-04-17 13:26:34 +0700

    Save model .joblib

--- machine_learning.py
+++ machine_learning.py
@@ -3,13 +3,15 @@ import pandas as pd
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline as ImbPipeline
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import classification_report, recall_score
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
-from lazypredict.Supervised import LazyClassifier
+import joblib
+
 # Load the dataset
 data = pd.read_csv("diabetes_prediction_dataset.csv")
 
@@ -56,28 +58,32 @@ preprocessor = ColumnTransformer([
 cls = ImbPipeline(steps=[
     ('preprocessor', preprocessor),
     ('smote', SMOTE(random_state=42)),  # SMOTE to handle class imbalance
-    ('model', RandomForestClassifier(class_weight='balanced'))  # Adjust weights if needed
+    ('model', LogisticRegression(random_state=42)) # Adjust weights if needed
 ])
 
 #Define parameters for GridSearchCV
 params = {
-    'model__criterion': ['gini', 'entropy', 'log_loss']
+    'model__penalty': ['l1', 'l2', 'elasticnet'],
+    'model__solver': ['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga']
 }
 
-# Perform grid search for hyperparameter tuning
-grid_search = GridSearchCV(estimator=cls, param_grid=params, cv=5, scoring='recall', verbose=3)
-
+# # Perform grid search for hyperparameter tuning
+grid_search = GridSearchCV(estimator=cls, param_grid=params, cv=5, scoring='recall', verbose=3, n_jobs=5)
 # Train the model using GridSearchCV
-models, prediction = grid_search.fit(x_train, x_test, y_train, y_test)
+grid_search.fit(x_train, y_train)
+
+print(grid_search.best_params_)
 
 # Predict on the test set
 y_predict = grid_search.predict(x_test)
 
 # Evaluate the model
 print(classification_report(y_test, y_predict))
-print("Recall Score:", recall_score(y_test, y_predict))
 
-# Optionally: Display confusion matrix for more detailed evaluation
-from sklearn.metrics import confusion_matrix
-print("Confusion Matrix:")
-print(confusion_matrix(y_test, y_predict))
+# Display confusion matrix for more detailed evaluation
+# from sklearn.metrics import confusion_matrix
+# print("Confusion Matrix:")
+# print(confusion_matrix(y_test, y_predict))
+
+#Save model
+# joblib.dump(grid_search.best_estimator_, "diabetes_model_logisticregression.joblib")
