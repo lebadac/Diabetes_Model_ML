3957b12689c7385811d93fbd283f427f645667a7
Ba Dac <lebadac@Bas-MacBook-Pro.local>
2025-04-18 16:24:50 +0700

    Take user input for diabetes prediction

--- client/client.py
+++ client/client.py
@@ -1,14 +1,25 @@
 import requests
 
+# Prompt the user for input
+age = int(input("Enter age: "))
+hypertension = int(input("Hypertension (1 for Yes, 0 for No): "))
+heart_disease = int(input("Heart disease (1 for Yes, 0 for No): "))
+bmi = float(input("Enter BMI: "))
+HbA1c_level = float(input("Enter HbA1c level: "))
+blood_glucose_level = float(input("Enter blood glucose level: "))
+gender = input("Enter gender (Male/Female/Other): ")
+smoking_history = input("Enter smoking history (never/former/current/not current/ever/No Info): ")
+
+# Prepare the data to be sent to the API
 data = {
-  "age": 58,
-  "hypertension": 1,
-  "heart_disease": 1,
-  "bmi": 35.7,
-  "HbA1c_level": 8.9,
-  "blood_glucose_level": 230,
-  "gender": "Male",
-  "smoking_history": "former"
+    "age": age,
+    "hypertension": hypertension,
+    "heart_disease": heart_disease,
+    "bmi": bmi,
+    "HbA1c_level": HbA1c_level,
+    "blood_glucose_level": blood_glucose_level,
+    "gender": gender,
+    "smoking_history": smoking_history
 }
 
 
